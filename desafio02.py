57bcb40b10dcf24dcff904a2495165f3019ea0be
Gustavo Henrique <gustavosobralmagalhaes@gmail.com>
2022-12-04 11:46:29 -0300

    Add files via upload

---
+++ desafio02.py
@@ -0,0 +1,8 @@
+print ('Olá estranho por favor preencha as informações abaixo')
+dia = int(input ('Dia = '))
+mes = str(input ('Mês = '))
+ano = int(input ('Ano = '))
+print ('Você nasceu no dia ', dia, 'de', mes, 'de', ano!)
+
+
+
