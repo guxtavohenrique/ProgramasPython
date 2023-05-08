99566457305a6a69dcd3f114010cad13b7470374
Gustavo Henrique <gustavosobralmagalhaes@gmail.com>
2023-05-08 19:05:15 -0300

    Add files via upload

---
+++ exe049.py
@@ -0,0 +1,10 @@
+n = int(input("Digite um número: "))
+t = 0
+for c in range(1, n + 1):
+    if n % c == 0:
+        print('\033[34m', end=' ')
+        t += 1
+    else:
+        print('\033[31m', end=' ')
+    print('{}'.format(c), end='')
+print('\n O número {} foi divisível {} vezes'.format(n, t))
